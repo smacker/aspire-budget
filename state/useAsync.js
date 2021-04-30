4c849a56f26ecbae2b38014f847494a03b09b9aa
Maxim Sukharev <max@smacker.ru>
2021-04-30 18:00:51 +0800

    make useAsync cancel promises

--- state/useAsync.js
+++ state/useAsync.js
@@ -1,21 +1,36 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback, useRef } from 'react';
 
 function useAsync(asyncFunction, immediate = true) {
   const [status, setStatus] = useState('idle');
   const [value, setValue] = useState(null);
   const [error, setError] = useState(null);
 
+  // promises don't support cancellation
+  // but a promise can be resolved only after a component was unmounted
+  // or two promises running at the same time can produce unexpected results
+  //
+  // we need to avoid updating the state in such cases
+  // for each promise we run, increase the ref id and assign it to the promise
+  // if id in ref doesn't equal by the time of resolution -> it was cancelled
+  const lastPromiseIdRef = useRef(0);
+
   const execute = useCallback(() => {
+    const promiseId = ++lastPromiseIdRef.current;
+
     setStatus('pending');
     setValue(null);
     setError(null);
 
     return asyncFunction()
       .then((response) => {
+        if (lastPromiseIdRef.current != promiseId) return;
+
         setValue(response);
         setStatus('success');
       })
       .catch((error) => {
+        if (lastPromiseIdRef.current != promiseId) return;
+
         setError(error);
         setStatus('error');
       });
@@ -25,6 +40,12 @@ function useAsync(asyncFunction, immediate = true) {
     if (immediate) {
       execute();
     }
+
+    // clean-up function is called before unmounting a component
+    return () => {
+      // use longer form to avoid warning
+      lastPromiseIdRef.current = lastPromiseIdRef.current + 1;
+    };
   }, [execute, immediate]);
 
   return { execute, status, value, error };
