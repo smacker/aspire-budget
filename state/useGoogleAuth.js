944436208c5ad34c580c2ea335835618a94cf87d
Maxim Sukharev <max@smacker.ru>
2021-04-30 17:38:47 +0800

    catch errors in google auth

--- state/useGoogleAuth.js
+++ state/useGoogleAuth.js
@@ -79,35 +79,46 @@ function useGoogleAuth() {
       Constants.appOwnership === 'expo'
         ? ANDROID_CLIENT_ID
         : ANDROID_STANDALONE_CLIENT_ID;
-    const resp = await fetch(
-      `https://oauth2.googleapis.com/token?client_id=${clientId}&refresh_token=${authData.refreshToken}&grant_type=refresh_token`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
+    try {
+      const resp = await fetch(
+        `https://oauth2.googleapis.com/token?client_id=${clientId}&refresh_token=${authData.refreshToken}&grant_type=refresh_token`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+        }
+      );
+
+      if (resp.status !== 200) {
+        console.error(`google refresh token status code ${resp.status}`);
+        await logout();
+        return;
       }
-    );
 
-    if (resp.status !== 200) {
-      console.error(`google refresh token status code ${resp.status}`);
-      await logout();
-      return;
+      const {
+        access_token: accessToken,
+        expires_in: expiryTime,
+      } = await resp.json();
+      setAuthData({
+        ...authData,
+        accessToken: accessToken,
+        expiryTime: new Date().getTime() + expiryTime * 1000,
+      });
+      setStatus(stateAuthorized);
+    } catch (e) {
+      console.error('AUTH ERROR', e);
     }
-
-    const {
-      access_token: accessToken,
-      expires_in: expiryTime,
-    } = await resp.json();
-    setAuthData({
-      ...authData,
-      accessToken: accessToken,
-      expiryTime: new Date().getTime() + expiryTime * 1000,
-    });
-    setStatus(stateAuthorized);
   }, [authData, setAuthData, logout]);
 
-  return [status, authData && authData.accessToken, login, logout, refresh];
+  return [
+    status,
+    // return token only when the status is authorized
+    status == stateAuthorized && authData && authData.accessToken,
+    login,
+    logout,
+    refresh,
+  ];
 }
 
 export default useGoogleAuth;
