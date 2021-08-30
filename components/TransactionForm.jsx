cbe8bcf3391d8fa1e99dd66f0e3b492ca8df553e
Maxim Sukharev <max@smacker.ru>
2021-08-30 23:07:22 +0800

    simplify account transfer

--- components/TransactionForm.jsx
+++ components/TransactionForm.jsx
@@ -11,9 +11,10 @@ import { AccountsGate, $accounts, addTransaction } from '../state/transactions';
 import { colors } from './constants';
 import { formatDate } from '../helpers/date';
 
+const accountTransferCategory = '↕️ Account Transfer';
 const accountTxCategories = [
   'Available to budget',
-  '↕️ Account Transfer',
+  accountTransferCategory,
   '🔢 Balance Adjustment',
   '➡️ Starting Balance',
 ];
@@ -30,24 +31,46 @@ function TransactionForm({ categoryInit = '', accountInit = '', back }) {
     amount: '',
     category: categoryInit,
     account: accountInit,
+    to: '',
     memo: '',
   };
-  const { control, handleSubmit, register, watch, setValue, formState } =
-    useForm({
-      mode: 'onChange',
-      defaultValues,
-    });
+  const {
+    control,
+    handleSubmit,
+    register,
+    unregister,
+    trigger,
+    watch,
+    setValue,
+    formState,
+  } = useForm({
+    mode: 'onChange',
+    defaultValues,
+  });
+  const [date, category, account, account2, inflow] = watch([
+    'date',
+    'category',
+    'account',
+    'account2',
+    'inflow',
+  ]);
   React.useEffect(() => {
     register('date', { required: true });
     register('account', { required: true });
     register('category', { required: true });
   }, [register]);
-  const [date, category, account, inflow] = watch([
-    'date',
-    'category',
-    'account',
-    'inflow',
-  ]);
+  React.useEffect(() => {
+    if (!accountInit || category !== accountTransferCategory) {
+      unregister('account2');
+      return;
+    }
+
+    register('account2', { required: true });
+    trigger('account2'); // force re-validation
+  }, [register, unregister, trigger, category, accountInit]);
+
+  // read from form state as it is a proxy
+  const { dirtyFields, isValid, isSubmitting } = formState;
 
   // setup popups state
   const [showDatePicker, setShowDatePicker] = useState(false);
@@ -58,9 +81,17 @@ function TransactionForm({ categoryInit = '', accountInit = '', back }) {
   //const [serverError, setServerError] = useState(null);
   const setServerError = () => null;
 
+  // FIXME addTransaction is an event now, not Fx
   const onAdd = async (data) => {
     try {
       await addTransaction(data);
+      if (data.account2) {
+        await addTransaction({
+          ...data,
+          account: data.account2,
+          inflow: !data.inflow,
+        });
+      }
     } catch (e) {
       console.error(e);
       setServerError('Could not submit transaction. Please try again.');
@@ -69,12 +100,11 @@ function TransactionForm({ categoryInit = '', accountInit = '', back }) {
     await back();
   };
 
-  // disabled={accountsStatus !== 'success'}
   const accountItemComponent = (
     <ListItem bottomDivider onPress={() => setShowAccountsList(true)}>
       <ListItem.Content style={styles.FormRow}>
         <Text>Account</Text>
-        <Text style={styles.DefaultValue(formState.dirtyFields.account)}>
+        <Text style={styles.DefaultValue(dirtyFields.account)}>
           {account || 'Select'}
         </Text>
       </ListItem.Content>
@@ -89,10 +119,14 @@ function TransactionForm({ categoryInit = '', accountInit = '', back }) {
           key={i}
           onPress={() => {
             setShowAccountsList(false);
-            setValue('account', account, {
-              shouldValidate: true,
-              shouldDirty: true,
-            });
+            setValue(
+              category !== accountTransferCategory ? 'account' : 'account2',
+              account,
+              {
+                shouldValidate: true,
+                shouldDirty: true,
+              }
+            );
           }}
         >
           <ListItem.Content>
@@ -103,12 +137,11 @@ function TransactionForm({ categoryInit = '', accountInit = '', back }) {
     </BottomSheet>
   );
 
-  //disabled={accountsStatus !== 'success'}
   const categoryItemComponent = (
     <ListItem bottomDivider onPress={() => setShowCategoriesList(true)}>
       <ListItem.Content style={styles.FormRow}>
         <Text>Category</Text>
-        <Text style={styles.DefaultValue(formState.dirtyFields.category)}>
+        <Text style={styles.DefaultValue(dirtyFields.category)}>
           {category || 'Select'}
         </Text>
       </ListItem.Content>
@@ -135,6 +168,18 @@ function TransactionForm({ categoryInit = '', accountInit = '', back }) {
         </ListItem>
       ))}
     </BottomSheet>
+  );
+
+  const toItemComponent = (
+    <ListItem bottomDivider onPress={() => setShowAccountsList(true)}>
+      <ListItem.Content style={styles.FormRow}>
+        <Text>{inflow ? 'From' : 'To'}</Text>
+        <Text style={styles.DefaultValue(dirtyFields.account2)}>
+          {account2 || 'Select'}
+        </Text>
+      </ListItem.Content>
+      <ListItem.Chevron />
+    </ListItem>
   );
 
   return (
@@ -191,13 +236,16 @@ function TransactionForm({ categoryInit = '', accountInit = '', back }) {
         <ListItem bottomDivider onPress={() => setShowDatePicker(true)}>
           <ListItem.Content style={styles.FormRow}>
             <Text>Date</Text>
-            <Text style={styles.DefaultValue(formState.dirtyFields.date)}>
+            <Text style={styles.DefaultValue(dirtyFields.date)}>
               {formatDate(date)}
             </Text>
           </ListItem.Content>
           <ListItem.Chevron />
         </ListItem>
         {categoryInit ? accountItemComponent : categoryItemComponent}
+        {accountInit && category === accountTransferCategory
+          ? toItemComponent
+          : null}
         <ListItem bottomDivider>
           <ListItem.Content style={[styles.FormRow, styles.MemoRow]}>
             <Controller
@@ -220,7 +268,7 @@ function TransactionForm({ categoryInit = '', accountInit = '', back }) {
         <Button
           title="Add transaction"
           onPress={handleSubmit(onAdd)}
-          disabled={!formState.isValid || formState.isSubmitting}
+          disabled={!isValid || isSubmitting}
         />
       </View>
       {showDatePicker && (
@@ -238,6 +286,9 @@ function TransactionForm({ categoryInit = '', accountInit = '', back }) {
         />
       )}
       {categoryInit ? accountListComponent : categoriesListComponent}
+      {accountInit && category === accountTransferCategory
+        ? accountListComponent
+        : null}
     </View>
   );
 }
