96b7b9df48422cc09bd24d84e56c11f99ce9cb32
Maxim Sukharev <max@smacker.ru>
2022-03-07 21:39:43 +0800

    add global effect error logger

--- state/app/domain.ts
+++ state/app/domain.ts
@@ -1,3 +1,14 @@
 import { createDomain } from 'effector';
 
 export const app = createDomain();
+
+app.onCreateEffect((effect) => {
+  effect.fail.watch(({ params, error }) => {
+    console.error(
+      `effect '${effect.name}' called with params`,
+      params,
+      'failed with error',
+      error
+    );
+  });
+});
