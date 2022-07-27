ebeb0377f99d8a643ccca5960247e78f5e99934c
Maxim Sukharev <max@smacker.ru>
2022-07-27 16:22:40 +0800

    hard-code locale for currency formatter

--- state/configuration/index.ts
+++ state/configuration/index.ts
@@ -10,19 +10,22 @@ export const loadCurrencyCodeFx = app.createEffect<void, string, Error>();
 export const setCurrencyCodeFx = app.createEffect<string, string, Error>();
 
 export const $locale = app.createStore<string>(Localization.locale || 'en-US');
-export const $currencyCode = app.createStore<string>(
-  Localization.currency || 'USD'
-);
+export const $currencyCode = app.createStore<string>('USD');
 
 export const $currencyFormatter = combine(
   $locale,
   $currencyCode,
-  (locale, currencyCode) =>
-    new Intl.NumberFormat(locale, {
+  (locale, currencyCode) => {
+    // polyfill doesn't support `currencyDisplay=narrowSymbol`
+    // so hard-code en-US locale to be able to display $ instead of US$
+    const q = new Intl.NumberFormat('en-US', {
       style: 'currency',
       currencyDisplay: 'symbol',
       currency: currencyCode,
-    })
+    });
+
+    return q;
+  }
 );
 
 export const $dateFormatter = $locale.map((locale) => {
