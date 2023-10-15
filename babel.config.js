e7a9f71e484cdc26e4bc3ac41d2a4aa37697cf4f
Maxim Sukharev <max@smacker.ru>
2023-10-15 14:29:55 +0800

    Fix react-navigation

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,10 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['module:react-native-dotenv', 'effector/babel-plugin'],
+    plugins: [
+      'module:react-native-dotenv',
+      'effector/babel-plugin',
+      'react-native-reanimated/plugin',
+    ],
   };
 };
